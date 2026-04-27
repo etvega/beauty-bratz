afde46c1ae9b7cd4cb4471d5bb5fc9fcc8153eef
etvega <etvega>
2026-04-27 00:02:27 -0500

    no borrar el registro del todo

--- app/models/models.py
+++ app/models/models.py
@@ -1,27 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
-
 
 # 🔹 MODELO: Categoría
 class Categoria(BaseModel):
-    id: int
+    id: Optional[int] = None
     nombre: str
     descripcion: Optional[str] = None
-
+    activo: bool = True  # <--- REQUISITO PUNTO 5: Para el histórico
 
 # 🔹 MODELO: Proveedor
 class Proveedor(BaseModel):
-    id: int
+    id: Optional[int] = None
     nombre: str
     telefono: str
     email: str
-
+    activo: bool = True  # <--- REQUISITO PUNTO 5: Para el histórico
 
 # 🔹 MODELO: Producto
 class Producto(BaseModel):
-    id: int
+    id: Optional[int] = None
     nombre: str
     precio: float
     cantidad: int
     categoria_id: int
     proveedor_id: int
+    activo: bool = True  # <--- REQUISITO PUNTO 5: Para el histórico
