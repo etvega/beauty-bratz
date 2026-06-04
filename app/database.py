87827777fef2d57d0caf8967fcdb810026bbaec9
etvega <etvega>
2026-06-04 10:01:39 -0500

    tablas

--- app/database.py
+++ app/database.py
@@ -4,13 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    engine = create_engine(DATABASE_URL)
-else:
-    engine = None
+print(f"DATABASE_URL encontrada: {DATABASE_URL is not None}")
+
+if not DATABASE_URL:
+    raise RuntimeError("❌ DATABASE_URL no está configurada")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
-) if engine else None
+)
