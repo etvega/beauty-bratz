355eafc307ddda5885b256521922e5063b3c7e11
etvega <etvega>
2026-06-04 07:41:37 -0500

    modificaciones

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.routes import router
+import os
 
 app = FastAPI(title="Beauty Bratz", version="1.0")
 
